40cfefecc2780893fc78f5dc0bea80a372358ed1
shatalley1-prog <263497495+shatalley1-prog@users.noreply.github.com>
2026-03-11 10:58:43 +0000

    feat: add session 01 linux navigation artifact

---
+++ harden.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# T1-M1-S02: SECURITY HARDENING AUTOMATION
+# Task: Restore Gold Standard permissions to restricted artifacts.
+
+# TODO: Add commands to secure ~/Vault/secrets.txt to 600
+# TODO: Add commands to secure /etc/shadow to 640
